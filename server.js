7968a45dd19f1d01337ae497c729a7ba0a811a71
develop331 <dexter@dexlabs.uk>
2026-05-19 20:43:56 +0000

    https

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const https = require('https');
 const Database = require('better-sqlite3');
 const crypto = require('crypto');
 const path = require('path');
