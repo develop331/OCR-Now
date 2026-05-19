96f3fe2598f35b28b723ecba447399c64fcc81d4
develop331 <dexter@dexlabs.uk>
2026-05-19 20:41:02 +0000

    remove double fs

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const multer = require('multer');
 const sharp = require('sharp');
 const { Server } = require('socket.io');
 // import { PORT, HTTPS_CONFIG } from "./src/config.js";
-const fs = require('fs')
 const { PORT, HTTPS_CONFIG } = fs.readFileSync('config.json', 'utf8');
 
 // Optional NSFW classifier (best-effort)
